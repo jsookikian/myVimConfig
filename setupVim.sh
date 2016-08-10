5a30c42b838d9830c5544dbeb6ca4ee7478de4a5
Jordan Sookikian <jsookikian@gmail.com>
2016-08-09 20:33:49 -0700

    updated shell script

--- setupVim.sh
+++ setupVim.sh
@@ -1,10 +1,27 @@
-CONFIG = 
+CONFIG_PATH=$(pwd)/"deepsea.vim"
+VIM="jordan"
+PLUGIN="plugin"
+echo $CONFIG_PATH
 
 cd $HOME
-if [ -d .vim ]
+#
+if [ ! -d $VIM ]
    then
-      echo "~/.vim exists"
-      if [ -d plugins ]
-      then
-         echo "plugins exists"
+      echo "~/$VIM does not exist"
+      echo "creating $VIM directory"
+      mkdir $VIM
+   fi
+cd $VIM
+pwd
 
+if [ ! -d $PLUGIN ]
+   then
+      echo "$PLUGIN directory does not exist"
+      echo "creating $PLUGIN directory"
+      mkdir $PLUGIN
+   fi
+
+cd $PLUGIN
+pwd
+cp $CONFIG_PATH . 
+echo 
